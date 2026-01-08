1253c5e9e850475ed290e387cabbe0077643d6a2
Tristifano <tristanbmyers@gmail.com>
2026-01-08 18:33:28 -0500

    altered while loop and added append method to todos list

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,7 +1,7 @@
 user_prompt = 'Enter a todo:'
-
+todos = []
 while True:
     todo =  input(user_prompt)
-    print(todo)
-    print("Next...")
+    print(todo.capitalize())
+    todos.append(todo)
 
