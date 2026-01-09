4dcb04655a4ccbc27f874dc6b9e0ed1d7b37abe8
Tristifano <tristanbmyers@gmail.com>
2026-01-09 13:07:49 -0500

    refactored while loop and added a match statement

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,9 +1,13 @@
-user_prompt = 'Enter a todo:'
-
 todos = []
 
 while True:
-    todo =  input(user_prompt)
-    print(todo.title())
-    todos.append(todo)
+    user_action =  input('Type add or show: ')
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+
 
